ce01a4ba388c971c933712281b91eb2bfd8ad3e5
schriftgestalt <georg.seifert@mac.com>
2024-04-29 09:53:01 +0200

    Simpler collectBubbleShapes

--- ShowKernBubbles.glyphsReporter/Contents/Resources/plugin.py
+++ ShowKernBubbles.glyphsReporter/Contents/Resources/plugin.py
@@ -17,14 +17,7 @@ import objc
 from GlyphsApp import *
 from GlyphsApp.plugins import *
 import traceback
-from dataclasses import dataclass, field
 
-@dataclass()
-class layerAttributes:
-	bubble: GSLayer = None
-	transform: tuple = None
-	children: list[int] = field(default_factory=list) # another layerAttributes?
-	depth: int = 0
 
 class ShowKernBubbles(ReporterPlugin):
 
@@ -41,84 +34,53 @@ class ShowKernBubbles(ReporterPlugin):
 			}]
 
 	@objc.python_method
-	def collectBubbleShapes(self, layer, theTransform=(1.0, 0.0, 0.0, 1.0, 0.0, 0.0), depth=0):
+	def collectBubbleShapes(self, layer, depth=0):
 		# Input layer, transform, and bubble pursuit level.
 		# Returns a layer attributes instance.
 		try:
-			m = Glyphs.font.selectedFontMaster
-			thePath = None
-			children = []
-			theMasterLayer = layer.parent.layers[m.id]
-			if theMasterLayer is not None:
-				if theMasterLayer.components:
-					for c in theMasterLayer.components:
-						children.append(self.collectBubbleShapes(c.componentLayer, c.transform, depth+1))
-			for l in layer.parent.layers:
-				if l.name == 'bubble' and l.master == m: # path
-					thePath = l.completeBezierPath
-					break
-			currentAttributes = layerAttributes(l, theTransform, children, depth)
-			return currentAttributes
+			if depth > 5:
+				return None
+
+			theMasterLayer = layer.parent.layers[layer.associatedMasterId]
+			if theMasterLayer is None:
+				return None
+
+			bubbleLayer = layer.parent.layerForName_masterId_('bubble', layer.associatedMasterId)
+			if bubbleLayer is None:
+				return None
+			bubblePath = bubbleLayer.bezierPath.copy()
+
+			for c in theMasterLayer.components:
+				componentPath = self.collectBubbleShapes(c.componentLayer, depth + 1)
+				if componentPath is None:
+					continue
+				componentPath.transformUsingAffineTransform_(c.pyobjc_instanceMethods.transform())
+				bubblePath.appendPath_(componentPath)
+
+			return bubblePath
 		except:
 			print('collectBubbleShapes error: ', traceback.format_exc())
 
 	@objc.python_method
-	def buildBubble(self, theAttributes, li, inheritedTransforms=[], lastDepth=0):
-		# receives bubble attributes, li (imaginary layer) to build a bubble, parent attributes.
-		# Adds bubble shape to the li.
-		try:
-			if theAttributes.children: # if there are components
-				for c in theAttributes.children: # c = attribute
-					if theAttributes.transform is not None:
-						inheritedTransforms.append(theAttributes.transform)
-					self.buildBubble(c, li, inheritedTransforms)
-			possibleBubble = theAttributes.bubble
-			if possibleBubble.name == 'bubble':
-				currentDepth = theAttributes.depth
-				bubbleCopy = possibleBubble.copy()
-				inheritedTransforms.append(theAttributes.transform)
-				for t in inheritedTransforms:
-					trans = NSAffineTransform()
-					trans.setTransformStruct_(t)
-					bubbleCopy.transform(trans)
-				for s in bubbleCopy.shapes:
-					li.shapes.append(s.copy())
-				for i in range(currentDepth-lastDepth):
-					if inheritedTransforms:
-						inheritedTransforms.pop(-1)
-				lastDepth = currentDepth
-		except:
-			print('buildBubble error: ', traceback.format_exc())
-
-	@objc.python_method
-	def inactiveLayerBackground(self, layer): # drawing for non-main glyphs
+	def drawBubble(self, layer):
 		try:
 			defaultColor = NSColor.colorWithCalibratedRed_green_blue_alpha_(0.5, 0.4, 1.0, 0.25)
 			defaultColor.set()
 
 			theBubbles = self.collectBubbleShapes(layer)
-			li = GSLayer()
-			self.buildBubble(theBubbles, li, [])
-
-			if li.bezierPath is not None: # if there're some contents in li
-				li.bezierPath.fill()
+			if theBubbles is not None:  # if there are some contents in li
+				theBubbles.fill()
 		except:
 			print('inactiveLayerBackground error: ', traceback.format_exc())
 
 	@objc.python_method
-	def background(self, layer): # drawing for the main glyph
-		try:
-			defaultColor = NSColor.colorWithCalibratedRed_green_blue_alpha_(0.5, 0.4, 1.0, 0.25)
-			defaultColor.set()
+	def inactiveLayerBackground(self, layer):  # drawing for non-main glyphs
+		self.drawBubble(layer)
 
-			theBubbles = self.collectBubbleShapes(layer)
-			li = GSLayer()
-			self.buildBubble(theBubbles, li, [])
-			if li.bezierPath is not None: # if there're some contents in li
-				li.bezierPath.fill()
-		except:
-			print('background error: ', traceback.format_exc())
-	
+	@objc.python_method
+	def background(self, layer):  # drawing for the main glyph
+		self.drawBubble(layer)
+
 	def doSomething_(self, sender): # unused
 		print('Just did something')
 
